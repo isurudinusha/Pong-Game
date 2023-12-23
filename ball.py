66dec4ae138fd7bbd17785c26d1bb67b0c12d978
Isuru Dinusha <isurudinusha@gmail.com>
2023-12-23 15:48:58 +0000

    Fix ball bounce and move speed

--- ball.py
+++ ball.py
@@ -18,11 +18,14 @@ class Ball(Turtle):
 
     def bounce_y(self):
         self.y_move *= -1
+        
 
     def bounce_x(self):
         self.x_move *= -1
+        self.move_speed *= 0.9
 
     def reset_position(self):
         self.goto(0,0)
         self.bounce_x()
+        self.move_speed = 0.1
 
