cd031e3f604158fd698d4c0cdb27b7bedf0af5c3
Isuru Dinusha <isurudinusha@gmail.com>
2023-12-23 15:43:24 +0000

    Add reset_position method to Ball class and integrate Scoreboard functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -15,6 +16,8 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
+
 
 screen.listen()
 screen.onkeypress(r_paddle.go_up, "Up")
@@ -36,6 +39,21 @@ while game_is_on:
     # Detect collision with paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+    
+    # Detect when right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+
+    # Detect when left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+    
+
+    
     
 
     
