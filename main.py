cd8c7655287969e6168c3fe89d695dd168cd621f
Isuru Dinusha <isurudinusha@gmail.com>
2023-12-23 15:29:06 +0000

    Fix ball bounce behavior

--- main.py
+++ main.py
@@ -31,7 +31,13 @@ while game_is_on:
     # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         # Needs to bounce
-        ball.bounce()
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    
+
     
 
 screen.exitonclick()
