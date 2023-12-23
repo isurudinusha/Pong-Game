35858516707a5d33af765df45dca879fcbb400fb
Isuru Dinusha <isurudinusha@gmail.com>
2023-12-23 15:24:33 +0000

    Add ball movement and bouncing functionality

--- main.py
+++ main.py
@@ -28,4 +28,10 @@ while game_is_on:
     screen.update()
     ball.move()
 
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # Needs to bounce
+        ball.bounce()
+    
+
 screen.exitonclick()
